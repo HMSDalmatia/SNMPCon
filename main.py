734a9e5524ba4b6d06a046878395955b60df32d9
HMSDalmatia <d.debicki03@gmail.com>
2025-04-07 16:15:14 +0200

    some more work for scanner to even  work. This commit is just to backup

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import functions
 
+data = []
+with open('config.txt','r') as file:
+    line = file.readline().strip()
+    while len(line) > 0:
+        if line[0] != '#':
+            data.append(line)
+    line = file.readline().strip()
+
+net_addr = data[0].split(".")
+mask = data[1].split(".")
+to_check=[]
+for i in mask:
+    helper_in_count = 255-int(i)
+    to_check.append(helper_in_count)
+
